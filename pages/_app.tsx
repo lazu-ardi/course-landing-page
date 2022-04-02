57bdebeb52dc761fab67e514ebcf39a01ceed264
Lazuardi <lazuardi_imani@pm.me>
2022-04-03 03:03:55 +0700

    feat: Implement Header and Footer components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
 
 export default MyApp
